a6730ea3b4d237827314d77e74779ec52e33e39d
Hassaan Tariq <hassaantariq584@gmail.com>
2024-08-24 03:35:11 +0500

    Load Products from the backend

--- data/products.js
+++ data/products.js
@@ -63,7 +63,31 @@ class Clothing extends Product
   
 }
 
-export const products = [
+export let products = [];
+export function loadProducts(fun)
+{
+  const xhr = new XMLHttpRequest();
+
+  xhr.addEventListener('load', () => {
+    products = JSON.parse(xhr.response).map((productDetails) => {
+
+      if(productDetails.type === 'clothing')
+      {
+        return new Clothing(productDetails);
+      }
+      return new Product(productDetails);
+    });
+
+    console.log('load products');
+    fun();
+  });
+
+  xhr.open('GET', 'https://supersimplebackend.dev/products');
+  xhr.send();
+
+}
+
+/*export const products = [
   {
     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
     image: "images/products/athletic-cotton-socks-6-pairs.jpg",
@@ -729,4 +753,4 @@ export const products = [
     return new Clothing(productDetails);
   }
   return new Product(productDetails);
-});
+});*/
